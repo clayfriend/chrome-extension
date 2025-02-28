fac1e19641dff3ca7fdbc9a5de440e40a774b9b9
clayfriend <muminov0101@mail.ru>
2025-02-28 18:45:23 +0500

    changing to mobile app

---
+++ config.js
@@ -0,0 +1,6 @@
+const firebaseConfig = {
+    databaseURL: "https://leads-tracker-app-4ff79-default-rtdb.asia-southeast1.firebasedatabase.app/"
+};
+
+// Export the config object so it can be used in other files
+export default firebaseConfig;
--- index.js
+++ index.js
@@ -1,38 +1,53 @@
-let myLeads = [];
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
+import {
+  getDatabase,
+  ref,
+  push,
+  onValue,
+  remove,
+  
+} from "https://www.gstatic.com/firebasejs/10.8.1/firebase-database.js";
+
+const firebaseConfig = {
+  databaseURL: "https://leads-tracker-app-4ff79-default-rtdb.asia-southeast1.firebasedatabase.app/",
+};
+
+console.log(firebaseConfig)
+
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+console.log(firebaseConfig.databaseURL);
+
+
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
 const deleteBtn = document.getElementById("delete-btn");
-const tabBtn = document.getElementById("tab-btn");
+const referenceInDB=ref(database,"lead")
 
-const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
 
-if (leadsFromLocalStorage) {
-  myLeads = leadsFromLocalStorage;
-  render(myLeads);
-}
 
-//TABS BUTTON FUNCTION
-tabBtn.addEventListener("click", function () {
-  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-    myLeads.push(tabs[0].url);
-    localStorage.setItem("myLeads", JSON.stringify(myLeads));
-    render(myLeads);
-  });
-});
+
+onValue(referenceInDB, function(snapshot){
+  console.log(snapshot)
+  const snapshotExist=snapshot.exists()
+  if(snapshotExist){
+    const snapshotValues=snapshot.val()
+    const leads = Object.values(snapshotValues)
+    render(leads)
+  }
+
+ 
+})
 
 deleteBtn.addEventListener("dblclick", function () {
-  localStorage.clear();
-  myLeads = [];
-  render(myLeads);
+  remove(referenceInDB)
+  ulEl.innerHTML=""
 });
 
 inputBtn.addEventListener("click", function () {
-  myLeads.push(inputEl.value);
+  push(referenceInDB, inputEl.value);
   inputEl.value = "";
-  localStorage.setItem("myLeads", JSON.stringify(myLeads));
-
-  render(myLeads);
 });
 
 function render(leads) {
